8e6413054294d55d6b90a5b38191bc7efd958f49
william <tobias.hulland@gmail.com>
2022-02-23 20:16:50 +0000

    interpolation-fix

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
-from types import NoneType
 import pygame
 import numpy
 import cv2
 import random
 import time
 import mediapipe
+
+from pygame import Vector2 as Vector2
 
 mp_drawing = mediapipe.solutions.drawing_utils
 mp_drawing_styles = mediapipe.solutions.drawing_styles
@@ -13,12 +14,10 @@ mp_hands = mediapipe.solutions.hands
 def lerp(a, b, c):
     return a + (b - a) * c
 
-
 class GameContainerClass:
     window = {
         "instance" : None,
-        "width" : 1280,
-        "height" : 720
+        "size" : Vector2(1280, 720)
     }
     camera = {
         "instance" : None,
@@ -26,10 +25,12 @@ class GameContainerClass:
     }
     components = {
         "player1" : {
-            "slider" : None
+            "slider" : None,
+            "target" : Vector2(0, 0)
         },
         "player2" : {
-            "slider" : None
+            "slider" : None,
+            "target" : Vector2(0, 0)
         }
     }
     running = False
@@ -40,30 +41,30 @@ class GameContainerClass:
         pygame.display.set_caption('Pong!')
         
         self.window["instance"] = pygame.display.set_mode(
-            (self.window["width"], self.window["height"])
+            (self.window["size"].x, self.window["size"].y)
         )
         self.window["instance"].fill((22, 33, 44))
         
         self.camera["instance"] = cv2.VideoCapture(0)
-        self.camera["instance"].set(3, self.window["width"])
-        self.camera["instance"].set(4, self.window["height"])
+        self.camera["instance"].set(3, self.window["size"].x)
+        self.camera["instance"].set(4, self.window["size"].y)
 
         self.camera["detector"] = mp_hands.Hands(
-            model_complexity = 0, 
+            model_complexity = 0,
             min_detection_confidence = 0.5, 
             min_tracking_confidence = 0.5
         )
 
         self.components["player1"]["slider"] = pygame.Rect(
-            self.window["width"] * 0.1,
-            self.window["height"] * 0.5,
+            self.window["size"].x * 0.1,
+            self.window["size"].y * 0.5,
             25,
             200
         )
 
         self.components["player2"]["slider"] = pygame.Rect(
-            self.window["width"] * 0.9,
-            self.window["height"] * 0.5,
+            self.window["size"].x * 0.9,
+            self.window["size"].y * 0.5,
             25,
             200
         )
@@ -90,16 +91,6 @@ class GameContainerClass:
 
         results = self.camera["detector"].process(frame)
 
-        # if results.multi_hand_landmarks:
-        #     for hand_landmarks in results.multi_hand_landmarks:
-        #         mp_drawing.draw_landmarks(
-        #             frame,
-        #             hand_landmarks,
-        #             mp_hands.HAND_CONNECTIONS,
-        #             mp_drawing_styles.get_default_hand_landmarks_style(),
-        #             mp_drawing_styles.get_default_hand_connections_style()
-        #         )
-
         surface = pygame.surfarray.make_surface(
             numpy.rot90(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
         )
@@ -109,29 +100,28 @@ class GameContainerClass:
         self.window["instance"].blit(surface, (0, 0)) 
 
         for i in range(0, 2):
-            if (results.multi_hand_landmarks == None):
-                break
-            elif (i + 1 > len(results.multi_hand_landmarks)):
-                break
-            
             player = self.components["player" + str(i + 1)]
-            controller = results.multi_hand_landmarks[i]
             
-            if controller:
-                x = (1 - controller.landmark[8].x) * self.window["width"]
-                y = controller.landmark[8].y * self.window["height"]
+            if (results.multi_hand_landmarks != None) and ((i + 1) <= len(results.multi_hand_landmarks)):
+                controller = results.multi_hand_landmarks[i]
+                
+                if controller:
+                    x = (1 - controller.landmark[8].x) * self.window["size"].x
+                    y = controller.landmark[8].y * self.window["size"].y
 
-                pygame.draw.circle(self.window["instance"], (255, 0, 0), (x, y), 20) 
+                    player["target"] = Vector2(x, y)
 
-                #player["slider"].x = 100
-                player["slider"].y = lerp(player["slider"].y, y, 0.3)
+                    pygame.draw.circle(self.window["instance"], (255, 0, 0), (x, y), 20)
 
-                pygame.draw.rect(
-                    self.window["instance"],
-                    (255, 0, 0), 
-                    player["slider"]
-                )
-            
+            #player["slider"].x = 100
+            player["slider"].y = lerp(player["slider"].y, player["target"].y, 0.3)
+
+            pygame.draw.rect(
+                self.window["instance"],
+                (230, 230, 230), 
+                player["slider"]
+            )
+                    
         pygame.display.update()
 
 
